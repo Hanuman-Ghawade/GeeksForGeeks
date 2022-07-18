0c84a96b4fb543827173980b034829de1d26208c
Hanuman Ghawade <hanuman.ghawade@excellarate.com>
2022-07-18 17:56:45 +0530

    changes done in programm

--- Min_max_sum.js
+++ Min_max_sum.js
@@ -5,24 +5,14 @@ long integers. */
 
 // output = 16 24
 const arr = [1,3,5,7,9];
-let sum=0;
+let sum1=0;
 let sum2=0;
-sum3=0;
-let text = "";
 for (let i = 0; i < arr.length-1; i++) {
-  text += ' ' + arr[i];
-  sum =sum+arr[i];
-
+  sum1 +=arr[i];
 }
 for (let i = 1; i < arr.length; i++) {
-    text += ' ' + arr[i];
-    sum2 =sum2+arr[i];
+    sum2 +=arr[i];
 
-  
   }
 
-console.log(text)
-console.log(sum,sum2);
-
-
-
+console.log(sum1,sum2);
