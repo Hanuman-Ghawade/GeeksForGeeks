120df38293e1597a2b2caec6d885da679aaafe41
Hanuman Ghawade <hanuman.ghawade@excellarate.com>
2022-07-19 11:37:49 +0530

    File Added

---
+++ Palindrome.js
@@ -0,0 +1,19 @@
+ // Palindrome String
+
+function isPalindrome (str){
+
+    text = ''
+    for (let i= str.length -1; i >= 0 ; i--) {
+        text += str[i]
+    }
+        
+    if (str === text) {
+        return "This is a Palindrome string";
+        
+    } else {
+        return  "This is not a Palindrome string";
+        
+    }
+}
+
+console.log(isPalindrome("Hanuman"))
