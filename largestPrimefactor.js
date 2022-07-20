b88bba7affe64d0e9bfe0decee538c0117ed7c06
Hanuman Ghawade <hanuman.ghawade@excellarate.com>
2022-07-20 17:35:40 +0530

    File Updated

--- largestPrimefactor.js
+++ largestPrimefactor.js
@@ -1,25 +1,16 @@
 // find out the largest prime factor in given number 
 
-// var max=0;
-const number = 24;
-var text = []
-for(var i=1;i<=number;i++){
-    if(number%i==0){
-    console.log(i)
-    text.push(i);
-    }    
-}
- var last_ele = text.length;
- 
-console.log(text)
-var highest_number = text[last_ele-1]
-// console.log(highest_number)
-var text_second = [];
-for(var i=1;i<highest_number;i++){
-    if(highest_number%i==0){
-    // console.log(i)
-    text_second.push(i);
+let number = 26;
+let primeBOI = 2 ;
+for(let i=0;i<=number;i++){
+    if(number % primeBOI == 0){
+        number /= primeBOI ;
+        console.log("***")
+    }    else {
+        console.log(primeBOI)
+        primeBOI ++  ;
+
     }
 }
-console.log(text_second)
-console.log(`The  largets prime factor of ${number} is ${text_second[text_second.length-1]}`)
+ console.log(primeBOI)
+
